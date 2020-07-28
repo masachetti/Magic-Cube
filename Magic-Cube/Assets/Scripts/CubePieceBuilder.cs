25a8f3422924330373f12e871b7ec9f707bb4bc8
masachetti <mateus.sachetti@gmail.com>
2020-07-27 22:32:48 -0400

    Updated CubePiece
    - Updated CubePiece prefab
    - Fixed Bug in Cube Piece Builder

--- Magic-Cube/Assets/Scripts/CubePieceBuilder.cs
+++ Magic-Cube/Assets/Scripts/CubePieceBuilder.cs
@@ -7,7 +7,7 @@ public class CubePieceBuilder : MonoBehaviour
 {
     public Color xColor, yColor, zColor;
     public Color borderColor;
-    public int size = 10;
+    public float size = 10;
     public int borderSize = 10;
  
 
@@ -27,27 +27,26 @@ public class CubePieceBuilder : MonoBehaviour
         {
             size = 1;
         }
+        
+        GameObject back = transform.Find("Back").gameObject;
+        GameObject up = transform.Find("Up").gameObject;
+        GameObject forward = transform.Find("Forward").gameObject;
+        GameObject down = transform.Find("Down").gameObject;
+        GameObject right = transform.Find("Right").gameObject;
+        GameObject left = transform.Find("Left").gameObject;
 
-        GameObject back = GameObject.Find("Back");
-        GameObject up = GameObject.Find("Up");
-        GameObject forward = GameObject.Find("Forward");
-        GameObject down = GameObject.Find("Down");
-        GameObject right = GameObject.Find("Right");
-        GameObject left = GameObject.Find("Left");
-
-        back.transform.position = Vector3.back * size/2;
-        up.transform.position = Vector3.up * size/2;
-        forward.transform.position = Vector3.forward * size/2;
-        down.transform.position = Vector3.down * size/2;
-        right.transform.position = Vector3.right * size/2;
-        left.transform.position = Vector3.left * size/2;
+        back.transform.localPosition = Vector3.back * size/2;
+        up.transform.localPosition = Vector3.up * size/2;
+        forward.transform.localPosition = Vector3.forward * size/2;
+        down.transform.localPosition = Vector3.down * size/2;
+        right.transform.localPosition = Vector3.right * size/2;
+        left.transform.localPosition = Vector3.left * size/2;
 
         up.transform.eulerAngles = Vector3.right * 90;
         down.transform.eulerAngles = Vector3.right * 90;
         right.transform.eulerAngles = Vector3.up * 90;
         left.transform.eulerAngles = Vector3.up * 90;
 
-        
         CubePieceFaceRender backF = back.GetComponent<CubePieceFaceRender>();
         backF.size = size;
         backF.borderSize = borderSize;
@@ -72,11 +71,15 @@ public class CubePieceBuilder : MonoBehaviour
         rightF.size = size;
         rightF.borderSize = borderSize;
         rightF.borderColor = borderColor;
-        rightF.fillColor = xColor;
+        rightF.fillColor = Color.black;
         CubePieceFaceRender leftF = left.GetComponent<CubePieceFaceRender>();
         leftF.size = size;
         leftF.borderSize = borderSize;
         leftF.borderColor = borderColor;
-        leftF.fillColor = Color.black;
+        leftF.fillColor = xColor;
+
+        BoxCollider collider = GetComponent<BoxCollider>();
+        collider.size = Vector3.one * size;
+
     }
 }
