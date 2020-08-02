e3682c6687562b4a8732227e7a703bbd698a6050
masachetti <mateus.sachetti@gmail.com>
2020-08-02 14:21:03 -0400

    Added camera scripts
    - Added script to moving camera.
    - Added script to handle with touch inputs.

---
+++ Magic-Cube/Assets/Scripts/MovingCamera.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{    
+    public float radius = 50;
+    public int moveSpeed = 1, rotateSpeed = 1;
+
+    private bool moving, rotating, movingClockwised;
+
+    private float radiansAlreadyMoved = 0.0f, realRadians;
+
+    private int right = 1;
+    private int left = 2;
+    private int top = 3;
+    private int down = 4;
+
+    private float anglesAlreadyRotated = 0.0f;
+
+    private Vector3 rotatingAxe;
+    private Vector3 targetPosition;
+    private char movingAxe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetCamera();
+        resetFlags();
+    }
+    void resetCamera(){
+        transform.position= new Vector3(0.0f, 0.0f, -radius);
+    }
+    void resetFlags(){
+        moving = false;
+        rotating = false;
+    }
+    public void moveRight(){
+        moveBySide(right);
+    }
+    public void moveLeft(){
+        moveBySide(left);
+    }
+    public void moveUp(){
+        moveBySide(top); 
+    }
+    public void moveDown(){
+        moveBySide(down);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving){
+            movingHandler();
+        }
+        if (rotating){
+            rotatingHandler();
+        }  
+    }
+
+    void movingHandler(){
+        int k = 1;
+        if (movingClockwised){
+            k = -1;
+        }
+        float radiansToMove = Mathf.PI * 2 * moveSpeed / 360;
+        if ((radiansAlreadyMoved + radiansToMove) >= (Mathf.PI/2)){
+            radiansToMove = (Mathf.PI/2) - radiansAlreadyMoved;
+            moving = false;
+        }
+        radiansAlreadyMoved = radiansAlreadyMoved + radiansToMove;
+        realRadians = realRadians + (radiansToMove*k);
+
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+        if (movingAxe == 'z'){
+            x = Mathf.Cos(realRadians) * radius;
+            y = Mathf.Sin(realRadians) * radius;
+        }
+        else if(movingAxe == 'x'){
+            y = Mathf.Sin(realRadians) * radius;
+            z = Mathf.Cos(realRadians) * radius;
+        }
+        else if(movingAxe == 'y'){
+            x = Mathf.Cos(realRadians) * radius;
+            z = Mathf.Sin(realRadians) * radius;
+        }
+
+        if (moving){
+            transform.position = new Vector3(x,y,z);        
+        }
+        else{
+            transform.position = targetPosition;
+        }
+    }
+
+    void rotatingHandler(){
+        float angleToRot = rotateSpeed;
+        if (anglesAlreadyRotated + rotateSpeed >=90){
+            rotating = false;
+            angleToRot = 90 - anglesAlreadyRotated;
+        }
+        transform.Rotate(rotatingAxe * angleToRot);
+        anglesAlreadyRotated += angleToRot;
+        
+    }
+
+    void moveBySide(int side){
+        if (!moving && !rotating){
+            anglesAlreadyRotated = 0.0f;
+            radiansAlreadyMoved = 0.0f;
+            if (side == right){
+                rotatingAxe = Vector3.down;
+            }
+            else if(side == left){
+                rotatingAxe = Vector3.up;
+            }
+            else if(side == top){
+                rotatingAxe = Vector3.right;
+            }
+            else{
+                rotatingAxe = Vector3.left;
+            }
+           
+
+            transform.Rotate(rotatingAxe*90);
+            Vector3 tempAngle = transform.eulerAngles;
+            transform.Rotate(-rotatingAxe*90);
+            targetPosition = getNewPositionByRotate(tempAngle);
+
+            setupMoving(transform.position, targetPosition);            
+
+            rotating = true;      
+            moving = true;        
+        }
+    }
+
+    Vector3 getNewPositionByRotate(Vector3 newRotate){
+        float x = newRotate.x;
+        float y = newRotate.y;
+        float z = newRotate.z;
+
+        float posX = 0.0f, posY = 0.0f, posZ = 0.0f;
+        if (Mathf.Abs(x) < 5){
+            if (Mathf.Abs(y-270) < 5){
+                posX = radius;
+            }
+            else if (Mathf.Abs(y-90) <5){
+                posX = -radius;
+            }
+            if(Mathf.Abs(y)<5){
+                posZ = -radius;
+            }
+            else if(Mathf.Abs(y-180)<5){
+                posZ = radius;
+            }
+        }
+        if (Mathf.Abs(z) < 5){
+            if (Mathf.Abs(x-270) < 5){
+                posY = -radius;
+            }
+            else if (Mathf.Abs(x-90) <5){
+                posY = radius;
+            }
+        }
+        return new Vector3(posX,posY,posZ);
+    }
+
+    void setupMoving(Vector3 init, Vector3 end){
+        targetPosition = end;
+        if (init.x == 0 && end.x == 0){
+            movingAxe = 'x';
+            if (init.y == 0){
+                movingClockwised = !(Mathf.Abs((init.z + end.y)) > 0);
+                realRadians = Mathf.PI* (1-(init.z/radius))/2;
+            }
+            if (init.z == 0){
+                movingClockwised = (Mathf.Abs((init.y + end.z)) > 0);
+                realRadians = Mathf.PI* ((init.y/radius))/2;
+            }
+        }
+        if (init.y == 0 && end.y == 0){
+            movingAxe = 'y';
+            if (init.x == 0){
+                movingClockwised = (Mathf.Abs(init.z + end.x) > 0);
+                realRadians = Mathf.PI* (2-(init.z/radius))/2;
+            }
+            if (init.z == 0){
+                movingClockwised = !(Mathf.Abs(init.x + end.z) > 0);
+                realRadians = Mathf.PI* (1-(init.x/radius))/2;
+            }
+        }
+        if (init.z == 0 && end.z == 0){
+            movingAxe = 'z';
+            if (init.x == 0){
+                movingClockwised = (Mathf.Abs(init.y + end.x) > 0);
+                realRadians = Mathf.PI* (2-(init.y/radius))/2;
+            }
+            if (init.y == 0){
+                movingClockwised = !(Mathf.Abs(init.x + end.y) > 0);
+                realRadians = Mathf.PI* (1-(init.x/radius))/2;
+            }
+        }
+    }
+
+}
---
+++ Magic-Cube/Assets/Scripts/TouchHandler.cs
@@ -0,0 +1,269 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchHandler : MonoBehaviour
+{
+
+    public float minToushSize = 40;
+
+    public float minRadiusSize = 70;
+
+    private int xFrame = 0;
+    private int yFrame = 1;
+    private int zFrame = 2;
+
+
+    private int plusX = 1;
+    private int minusX = 2;
+    private int plusY = 3;
+    private int minusY = 4;
+
+    private int plusZ = 5;
+    private int minusZ = 6;
+
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private ArrayList points = new ArrayList();
+    private GameObject actualClickedPiece;
+
+    private bool directionChosen;
+
+    private bool capturingMove = false;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getTouchInfo();
+    }
+
+    void getTouchInfo(){
+        if(Input.touchCount > 0){
+            Touch touch = Input.touches[0];
+            switch (touch.phase){
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    Ray ray = Camera.main.ScreenPointToRay(startPos);
+                    RaycastHit hit;
+                    points = new ArrayList();
+                    if(Physics.Raycast(ray, out hit)) {
+                        directionChosen = false;
+                        capturingMove = true;
+                        actualClickedPiece = hit.collider.gameObject;
+                        print(hit.transform.InverseTransformPoint(hit.point));
+                        print(actualClickedPiece.transform.position);
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (capturingMove){
+                        points.Add(touch.position);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (capturingMove){
+                        endPos = touch.position;
+                        directionChosen = true;
+                        capturingMove = false;
+                    }
+                    break;
+            }   
+        }
+        if (directionChosen){
+            int rotateAction = getTouchRotateAction();
+            // print(rotateAction);
+            int camSide = getCamSide();
+            int camFrame = getCamFrame();
+            int angleRotated = getCamRotate();
+            int squareRotated = (angleRotated%180)/90;
+            int inverseRotated = 0;
+            if (angleRotated >= 180){
+                inverseRotated = 1;
+            }
+
+            if (rotateAction == plusY){
+                int rotatingAxe;
+                if (squareRotated == 1){
+                    rotatingAxe = 1;
+                } else if(camSide == xFrame){
+                    rotatingAxe = 2;
+                }else{
+                    rotatingAxe = 0;
+                }
+                // int k = 0;
+                // if ((camSide + camFrame)%4 == 0){
+                //     k = 2;
+                // }else if ((camSide + camFrame) >= 2){
+                //     k = squareRotated;
+                // }
+                // int clockwised = (k + inverseRotated)%2;
+                int clockwised = 0;
+                if (camSide + camFrame == 2){
+                    clockwised = 1;
+                } 
+                int pieceFrame = getPieceFrame(rotatingAxe);
+                executeCubeAction(rotatingAxe, clockwised != 0, pieceFrame);
+            }else if (rotateAction == minusY){
+
+            }else if (rotateAction == plusX){
+                
+            }else if (rotateAction == minusX){
+                
+            }else if (rotateAction == plusZ){
+                bool _clockwised = !(camFrame != 0);
+                if (camSide == yFrame){_clockwised = !_clockwised;}
+                executeCubeAction(camSide, _clockwised , camFrame);
+            }else if(rotateAction == minusZ){
+                bool _clockwised = camFrame != 0;
+                if (camSide == yFrame){_clockwised = !_clockwised;}
+                executeCubeAction(camSide, _clockwised, camFrame);
+            }
+            directionChosen =false;
+        }
+    }
+
+    private GameObject getNeighborPiece(Vector2 _adder){
+        GameObject _piece = actualClickedPiece;
+        Vector2 _pos = new Vector2(startPos.x,startPos.y);
+        
+        while ((_piece == actualClickedPiece) && (_pos.x<Screen.width) && (_pos.x>0) && (_pos.y<Screen.height) && (_pos.y>0)){
+            Ray ray = Camera.main.ScreenPointToRay(_pos);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)) {
+                _piece = hit.collider.gameObject;
+            }
+            _pos += _adder;
+        }
+        return _piece;
+    }
+    
+    GameObject getRightPiece(){
+        return getNeighborPiece(new Vector2(10, 0));
+    }
+
+    GameObject getLeftPiece(){
+        return getNeighborPiece(new Vector2(-10, 0));
+    }
+    GameObject getUpPiece(){
+        return getNeighborPiece(new Vector2(0, 10));
+    }
+    GameObject getDownPiece(){
+        return getNeighborPiece(new Vector2(0, -10));
+    }
+
+    int getCamSide(){
+        Vector3 camPos = Camera.main.transform.position;
+        if (camPos.x != 0){
+            return xFrame;
+        }
+        else if (camPos.y != 0){
+            return yFrame;
+        }
+        else {
+            return zFrame;
+        }
+    }
+
+    int getCamFrame(){
+        Vector3 camPos = Camera.main.transform.position;
+        if (camPos.x > 0 || camPos.y > 0 || camPos.z > 0){
+            return 0;
+        }
+        else {
+            return 2;
+        }
+    }
+
+    int getCamRotate(){
+        Vector3 eulerRotate = Camera.main.transform.eulerAngles;
+        return (int)eulerRotate.z;
+    }
+
+    void executeCubeAction(int axe, bool clockwise, int frame){
+        GameObject cube = GameObject.Find("Cube");
+        CubeBuilder cubeScript = cube.GetComponent<CubeBuilder>();
+        cubeScript.rotate(axe, clockwise, frame);
+    }
+
+    int getTouchRotateAction(){
+        Vector2 center = startPos + (endPos - startPos)/2;
+        float radius = Mathf.Sqrt(Mathf.Pow(((center - startPos).x),2) + Mathf.Pow(((center - startPos).y),2));
+        ArrayList angles = new ArrayList();
+
+        bool verticalMove = true, horizontalMove = true, rotateMove = true;
+        foreach (Vector2 item in points){
+            if (Mathf.Abs(radius - Mathf.Sqrt(Mathf.Pow(((center - item).x),2) + Mathf.Pow(((center - item).y),2))) > minRadiusSize){
+                rotateMove = false;
+            }
+            else{
+                Vector2 tempPoint = item - center;
+                float tempAngle = Mathf.Acos(tempPoint.x/Mathf.Sqrt((tempPoint.x*tempPoint.x)+(tempPoint.y*tempPoint.y)))*180/Mathf.PI;
+                if (tempPoint.y < 0){
+                    tempAngle = 360 - tempAngle;
+                }
+                angles.Add(tempAngle);
+            }
+            if (Mathf.Abs(startPos.x - item.x)>minToushSize){
+                verticalMove = false;
+            }
+            if (Mathf.Abs(startPos.y - item.y)>minToushSize){
+                horizontalMove = false;
+            }
+        }
+        if (verticalMove){
+            if (endPos.y > startPos.y){
+                return plusY;
+            }
+            else{
+                return minusY;
+            }
+        }
+        if (horizontalMove){
+            if(endPos.x > startPos.x){
+                return plusX;
+            }
+            else{
+                return minusX;
+            }
+        }
+        if (rotateMove){
+            int plusAngleCount = 0, minusAngleCount = 0;
+            for(int i=1; i< angles.Count; i++){
+                if((float)angles[i] > (float)angles[i-1]){
+                    plusAngleCount += 1;
+                }else{
+                    minusAngleCount += 1;
+                }
+            }
+            if (plusAngleCount > minusAngleCount){
+                return plusZ;
+            }
+            else{
+                return minusZ;
+            }
+        }   
+        return 0;
+    }
+
+    int getPieceFrame(int rotAxe){
+        CubePieceBuilder pieceScript = actualClickedPiece.GetComponent<CubePieceBuilder>();            
+
+        if (rotAxe == 0){
+            return (((int)Mathf.Round(actualClickedPiece.transform.position.x/(pieceScript.size)) -1)*(-1));
+        }
+        else if (rotAxe == 1){
+            return (int) (Mathf.Round(actualClickedPiece.transform.position.y/(pieceScript.size)) - 1)*(-1);
+        }
+        else{
+            return (int) (Mathf.Round(actualClickedPiece.transform.position.z/(pieceScript.size)) - 1)*(-1);
+        }
+    }
+
+}
